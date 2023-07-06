3a666ef1445e821e269b6a0d1e6b2b7a54bb2220
Andy Martinez <amartinez@facturecolombia.com>
2023-07-06 16:59:28 -0500

    adicion de id a vehiculos

--- Entities/Models/Vehicles.cs
+++ Entities/Models/Vehicles.cs
@@ -8,7 +8,7 @@ public class Vehicles
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
-    
+    public string IdUser { get; set; }
     public string Type { set; get; }
     public string Plaque { set; get; }
     public int Model { set; get; }
